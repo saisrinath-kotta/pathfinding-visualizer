1bba5699213e276ca416f9018c45033bc78caa43
saisrinath-kotta <srinathsai076@gmail.com>
2026-05-24 19:42:36 +0530

    Updating GraphInput

--- src/components/Graph/GraphInput.js
+++ src/components/Graph/GraphInput.js
@@ -1,33 +1,82 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function GraphInput({ onGenerate }) {
+function GraphInput({
+  onGenerate,
+  onRunBFS
+}) {
 
   const [input, setInput] = useState("");
 
-  const handleGenerate = () => {
+  const [startNode, setStartNode] =
+    useState("");
+
+  const [endNode, setEndNode] =
+    useState("");
+
+  function handleGenerate() {
     onGenerate(input);
-  };
+  }
+
+  function handleRunBFS() {
+
+    onRunBFS(
+      input,
+      startNode,
+      endNode
+    );
+  }
 
   return (
-    <div>
+    <div style={{ marginTop: "40px" }}>
 
       <h2>Graph Input</h2>
 
       <textarea
-        rows="10"
+        rows="8"
         cols="30"
-        placeholder="Enter edges like:
-1 2
+        placeholder={`1 2
 1 3
-2 4"
+2 4
+4 5`}
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) =>
+          setInput(e.target.value)
+        }
       />
 
-      <br />
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Start Node"
+        value={startNode}
+        onChange={(e) =>
+          setStartNode(e.target.value)
+        }
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="End Node"
+        value={endNode}
+        onChange={(e) =>
+          setEndNode(e.target.value)
+        }
+      />
+
+      <br /><br />
 
       <button onClick={handleGenerate}>
         Generate Graph
+      </button>
+
+      <button
+        onClick={handleRunBFS}
+        style={{ marginLeft: "10px" }}
+      >
+        Run BFS
       </button>
 
     </div>
